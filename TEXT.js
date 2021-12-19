4441f780972a6fe045fdcb165d7dad5320274fa5
TCGoingW <84383083+TCGoingW@users.noreply.github.com>
2021-12-19 21:54:16 +0800

    Update TEXT.js

--- TEXT.js
+++ TEXT.js
@@ -431,7 +431,7 @@ q1a2.addEventListener("click", resultI);
 
 //#TODO: Define quiz functions here
 function setDisabled() {
-    document.getElementsByClassName('button1').disabled = false;
+    document.getElementsByClass("button1").disabled = false;
 }
 
 function resultE() {
